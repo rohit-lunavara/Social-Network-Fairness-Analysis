3a390f3079d8964387a46404930254131d9c1a04
rohit-lunavara <rohit.lunavara@gmail.com>
2019-11-27 02:46:12 -0500

    Added fairness inducing algorithm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import uniform, seed
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 import pickle
@@ -16,6 +17,23 @@ for i in G.nodes :
         else :
             G.edges[i, j]['infl_prob'] = 0.1
 
+# Obtaining a list of all regions
+
+region_list = []
+for i in G.nodes :
+    if G.nodes[i]['region'] not in region_list :
+        region_list.append(G.nodes[i]['region'])
+
+#print(region_list)
+
+# Obtaining total number of people in each region
+
+region_total = {}
+for i in G.nodes :
+    region_total[G.nodes[i]['region']] = region_total.get(G.nodes[i]['region'], 0) + 1
+
+#print(region_total.items())
+
 # Same regions
 #print(G.nodes[0])
 #print(G.nodes[1])
@@ -30,15 +48,15 @@ for i in G.nodes :
 #for (u, v, prob) in G.edges.data('infl_prob') :
 #   print(u, v, prob)
 
-# Independent Cascade function
-def independent_cascade(G, seeds, ic_iter = 50) :
+# Task 1.1 - Independent Cascade function
+def independent_cascade(G, seeds, get_region_spread = True, ic_iter = 500) :
     """
     Input : Graph, seeds and number of Monte-Carlo simulations
     Output : Average number of nodes influenced by the seed nodes
     """
 
     # Loop over the number of iterations 
-    spread = []
+    spread, region_spread = [], []
     for i in range(ic_iter) :
 
         # Simulate propagation process
@@ -63,15 +81,41 @@ def independent_cascade(G, seeds, ic_iter = 50) :
                 # Add newly activated nodes to the set of activated nodes
                 activated += new_active
 
+
+        # Proportion of people from each region
+        if get_region_spread :
+            region_iter = {}
+            for i in region_list :
+                region_iter[i] = 0
+            for i in activated :
+                region_iter[G.nodes[i]['region']] = region_iter.get(G.nodes[i]['region'], 0) + 1
+            for k, v in region_iter.copy().items() :
+                region_iter[k] = round(v / region_total[k], 4)
+            #print(region_iter, end = '\n\n')
+            region_spread.append(region_iter)
+
         spread.append(len(activated))
 
-    return np.mean(spread)
+    # Averaging over all iterations
+    if get_region_spread :
+        region_mean = {}
+        for i in region_list :
+            region_mean[i] = 0
+        for i in range(len(region_spread)) :
+            for region in region_list :
+                region_mean[region] += (region_spread[i][region])
+        for i in region_mean :
+            region_mean[i] = round(region_mean[i] / ic_iter, 8)
+        #print(region_mean)
+        return np.mean(spread), region_mean
+    else :
+        return np.mean(spread)
 
 #S = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 #mean_spread = independent_cascade(G, S)
 #print(mean_spread)
 
-def greedy(G, k, g_iter = 1) :
+def greedy(G, k, ic_iter = 10) :
     """
     Input : Graph, number of seeds and number of Monte-Carlo simulations
     Output : Optimal seed set, resulting spread, time for each iteration
@@ -86,11 +130,8 @@ def greedy(G, k, g_iter = 1) :
         best_spread = 0
         for j in (set(G.nodes) - set(seeds)) :
 
-            # Processing message
-            #print('Processing...')
-
             # Get the spread
-            s = independent_cascade(G, seeds + [j])
+            s = independent_cascade(G, seeds + [j], get_region_spread = True, ic_iter = ic_iter)
 
             # Update the winning node and spread so far
             if s > best_spread : best_spread, node = s, j
@@ -104,17 +145,104 @@ def greedy(G, k, g_iter = 1) :
 
     return (seeds, spread, timelapse)
 
-greedy_output = greedy(G, 25)
+# Task 1.2 - Compute Greedy algorithm output
 
-print('\nSeeds :', greedy_output[0], '\n')
-print('\nSpread :', greedy_output[1], '\n')
-print('\nTimelapse :', greedy_output[2], '\n')
+#greedy_output = greedy(G, 25)
+
+#print('\nSeeds :', greedy_output[0], '\n')
+#print('\nSpread :', greedy_output[1], '\n')
+#print('\nTimelapse :', greedy_output[2], '\n')
 #print(greedy_output)
 
-def max_fair_alloc(G, k, mfa_iter = 1) :
+# Task 1.3 - Compute proportion of people receiving information for each region averaged across 500 simulations.
+
+#seeds = [19, 264, 17, 13, 265, 282, 263, 460, 18, 296, 26, 240, 155, 136, 266, 82, 30, 409, 12, 268, 424, 74, 313, 29, 399]
+#mean_spread, region_spread = independent_cascade(G, seeds)
+#print(mean_spread)
+#plt.bar(region_spread.keys(), region_spread.values())
+#plt.xticks(region_list, region_list, rotation = 90)
+#plt.margins(0.2)
+#plt.gcf().subplots_adjust(bottom=0.15)
+#plt.tight_layout()
+#plt.show()
+
+#for k,v in region_spread.items() :
+#    print(v, k)
+
+# Task 2.1 - Fairness
+
+def max_fair_alloc(G, k, precision = 2, ic_iter = 10) :
     """
-    Input : Graph, number of seeds and number of Monte-Carlo simulations
+    Input : Graph, number of seeds, precision of variance and number of Monte-Carlo simulations
     Output : Maximum fairness seed set, resulting spread, time for each iteration
     """
 
-    pass
+    seeds, spread, timelapse, cumulative_variance, start_time = [], [], [], [], time.time()
+
+    # Find k nodes with largest marginal gain
+    for _ in range(k) :
+
+        # Loop over nodes that are not yet in seed set to find biggest marginal gain
+        records = []
+        for j in (set(G.nodes) - set(seeds)) :
+
+            # Get the spread
+            s, region_spread = independent_cascade(G, seeds + [j], get_region_spread = True, ic_iter = ic_iter)
+
+            # Store records for later
+            variance = round(np.var(list(region_spread.values())), precision)
+            records.append(tuple((seeds + [j], s, variance)))
+
+        # Sort the records to minimize the variance and maximize spread
+
+        # First, sort to minimize variance
+        records.sort(key = lambda x : x[2])
+
+        # Second, reverse sort only those records which have equal variance
+        minval = records[0][2]
+        selected_records, minindex = [], 0
+        for i in range(len(records)) :
+            if records[i][2] == minval : minindex = i
+        selected_records = records[0 : minindex + 1]
+        selected_records.sort(key = lambda x : x[1], reverse = True)
+
+        # Extract the best record
+        best_record = selected_records.pop(0)
+        print(best_record)
+        print('Seeds :', best_record[0])
+        node = best_record[0].pop(-1)
+        print('Added node :', node)
+        fair_spread = best_record[1]
+        print('Spread with added node :', fair_spread)
+        node_var = best_record[2]
+        print('Variance with added node :', node_var, end = '\n\n')
+
+        # Add the selected node to the seed set
+        seeds.append(node)
+
+        # Add estimated spread, elapsed time and variance upon adding each node
+        spread.append(fair_spread)
+        timelapse.append(round(time.time() - start_time, 2))
+        cumulative_variance.append(node_var)
+
+    return (seeds, spread, timelapse, cumulative_variance)
+
+# Task 2.1 - Compute Fairness algorithm output
+
+fairness_output = max_fair_alloc(G, 25, precision = 4, ic_iter = 500)
+
+print('\nSeeds :', fairness_output[0], '\n')
+print('\nSpread :', fairness_output[1], '\n')
+print('\nTimelapse :', fairness_output[2], '\n')
+print('\nVariance :', fairness_output[3], '\n')
+print(fairness_output)
+
+seeds = fairness_output[0]
+mean_spread, region_spread = independent_cascade(G, seeds, ic_iter = 500)
+print(mean_spread)
+plt.bar(region_spread.keys(), region_spread.values())
+plt.xticks(region_list, region_list, rotation = 90)
+plt.margins(0.2)
+plt.gcf().subplots_adjust(bottom=0.15)
+plt.tight_layout()
+plt.show()
--- tut.py
+++ tut.py
@@ -1,29 +1,39 @@
-import numpy
-import pickle
-import networkx as nx
+import statistics
+import numpy as np
 
-G = nx.DiGraph()
-G.add_edges_from([(0, 3), (1, 2), (2, 1), (1, 3)])
-G.nodes[0]['region'] = 'b'
-G.nodes[1]['region'] = 'a'
-G.nodes[2]['region'] = 'b'
-G.nodes[3]['region'] = 'a'
+sample1 = [1, 2, 1, 2, 1, 2]
+sample2 = [1, 2, 2, 2, 2, 1]
+sample3 = [2, 2, 2, 2, 2, 2]
 
-for i in range(G.number_of_nodes()) :
-    for j in G.adj[i] :
-        if G.nodes[i]['region'] == G.nodes[j]['region'] :
-            G.edges[i, j]['infl_prob'] = 0.2
-        else :
-            G.edges[i, j]['infl_prob'] = 0.1
+#print(statistics.variance(sample1))
+#print(statistics.variance(sample2))
+#print(statistics.variance(sample3))
+print()
 
-#print(G.edges)
-#for (u, v, prob) in G.edges.data('infl_prob') :
-#    print(u, v, prob)
+d = {'northwest_antelope_valley': 0.0, 'northeast_antelope_valley': 0.0148616, 'lancaster': 0.13805960000000003, 'quartz_hill': 0.006803399999999999, 'lake_los_angeles': 0.11412759999999961, 'northwest_palmdale': 0.022399999999999986, 'leona_valley': 0.038, 'palmdale': 0.15064660000000005, 'desert_view_highlands': 0.0, 'sun_village': 0.007203599999999999, 'littlerock': 0.087, 'acton': 0.0, 'southeast_antelope_valley': 0.0}
+#print(np.var(list(d.values())))
 
-#for node in G.nodes :
-#    print(len(G.successors(node)))
-#    for n in G.successors(node) :
-#        print(node, n)
-G = pickle.load(open('graph.pickle', 'rb'))
-
-print(len(G.edges))
+records = [
+    ([0], 10, 0.1),
+    ([1], 11, 0.2),
+    ([2,3,4,5], 12, 0.1)
+]
+records.append(tuple([0], 10, 0.1))
+records.sort(key = lambda x : x[2])
+#print(records)
+minval = records[0][2]
+minindex = 0
+for i in range(len(records)) :
+    if records[i][2] == minval : minindex = i
+#print(minindex)
+new_records = records[:minindex+1]
+new_records.sort(key = lambda x : x[1], reverse = True)
+best_record = new_records.pop(0)
+print(best_record)
+node = best_record[0].pop(-1)
+print('Added node :', node)
+spread = best_record[1]
+print('Spread with added node :', spread)
+#print(np.var(sample1))
+#print(np.var(sample2))
+#print(np.var(sample3))
